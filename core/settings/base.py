e70aed4e9c1160cb577e74ea3de131edd7e42d3d
Diego Fonseca González <diego.fonseca@ab-inbev.com>
2025-06-17 10:07:13 -0500

    update project

--- core/settings/base.py
+++ core/settings/base.py
@@ -48,8 +48,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "wagtail_dp_tools",
-    "wagtail_dp_tools.csrgenerator",
     "bot",
 ]
 
